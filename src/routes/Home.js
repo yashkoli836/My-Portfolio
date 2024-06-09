b8da4306c0ece3abca6c08bb1b26c2a215219ab1
yashmahor <yashkoli836@gmail.com>
2024-06-09 11:36:38 +0530

    some changes

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,12 +3,14 @@ import Navbar from '../components/Navbar'
 import HeroImg from '../components/HeroImg'
 import Footer from '../components/Footer'
 import Work from '../components/Work'
+import AboutContent from '../components/AboutContent'
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
         <HeroImg/>
+        <AboutContent/>
         <Work/>
         <Footer/>
     </div>
